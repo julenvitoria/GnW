22dbf6dec4e2dfa41176efe2994a29dff81cff1e
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2021-12-07 02:20:35 +0100

    Update 2.2.2-cfw-retro-go-1mb.sh

--- scene/2.2.2-cfw-retro-go-1mb.sh
+++ scene/2.2.2-cfw-retro-go-1mb.sh
@@ -94,7 +94,7 @@ case $menuitem in
         --title "Compilar Retro-Go" \
         --msgbox "Proceso cancelado." 0 0
     fi
-    ./scene/2.2.3-cfw-retro-go-16mb.sh
+    ./scene/2.2.2-cfw-retro-go-1mb.sh
     clear;;
   3)clear
     dialog --backtitle "G&W $consola - Utilidades de flasheo" \
