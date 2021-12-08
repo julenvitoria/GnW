f66b00c57de5ca299b7f19d875f6c21a1a49d1d7
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2021-12-08 14:34:00 +0100

    menu update

--- instalacion/1.2-cambio-consola.sh
+++ instalacion/1.2-cambio-consola.sh
@@ -17,7 +17,11 @@ case $menuitem in
     sed -i 's/^consola=.*$/'consola=\""mario"\"'/g' ./2-menu-scene-mario.sh
     sed -i 's/^consola=.*$/'consola=\""mario"\"'/g' ./scene/2.1-backup-restauracion.sh
     sed -i 's/^consola=.*$/'consola=\""mario"\"'/g' ./scene/2.2-retro-go-mario.sh
-    sed -i 's/^consola=.*$/'consola=\""mario"\"'/g' ./menu.sh
+    sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.1-retro-go-1mb-mario.sh
+    sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.2-cfw-retro-go-1mb-mario.sh
+    sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.3-cfw-retro-go-16mb-mario.sh
+    sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.4-cfw-retro-go-64mb-mario.sh
+	sed -i 's/^consola=.*$/'consola=\""mario"\"'/g' ./menu.sh
     echo "Opcion mario aplicada"
     sleep 2
     clear;;
@@ -26,6 +30,10 @@ case $menuitem in
     sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./2-menu-scene-zelda.sh
     sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.1-backup-restauracion.sh
     sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2-retro-go-zelda.sh
+    sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.1-retro-go-4mb-zelda.sh
+    sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.2-cfw-retro-go-4mb-zelda.sh
+    sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.3-cfw-retro-go-16mb-zelda.sh
+    sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.4-cfw-retro-go-64mb-zelda.sh
     sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./menu.sh
     echo "Opcion zelda aplicada"
     sleep 2
--- menu.sh
+++ menu.sh
@@ -64,8 +64,29 @@ case $menuitem in
     sed -i 's/^usuario=.*$/'usuario=\""$varname"\"'/g' ./scene/2.2.5-actualizacion-retro-go.sh
     sed -i 's/^usuario=.*$/'usuario=\""$varname"\"'/g' ./scene/2.2.6-actualizacion-parche.sh
     sed -i 's/^usuario=.*$/'usuario=\""$varname"\"'/g' ./menu.sh
-    read -n 1 -s -r -p "Proceso realizado. Presiona cualquier tecla para continuar"
-    ./menu.sh
+	if $consola=mario then
+		sed -i 's/^consola=.*$/'consola=\""mario"\"'/g' ./1-menu-instalacion.sh
+		sed -i 's/^consola=.*$/'consola=\""mario"\"'/g' ./2-menu-scene-mario.sh
+		sed -i 's/^consola=.*$/'consola=\""mario"\"'/g' ./scene/2.1-backup-restauracion.sh
+		sed -i 's/^consola=.*$/'consola=\""mario"\"'/g' ./scene/2.2-retro-go-mario.sh
+		sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.1-retro-go-1mb-mario.sh
+		sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.2-cfw-retro-go-1mb-mario.sh
+		sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.3-cfw-retro-go-16mb-mario.sh
+		sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.4-cfw-retro-go-64mb-mario.sh
+		sed -i 's/^consola=.*$/'consola=\""mario"\"'/g' ./menu.sh
+		read -n 1 -s -r -p "Proceso realizado. Presiona cualquier tecla para continuar"
+    fi
+	if $consola=zelda then
+	    sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./1-menu-instalacion.sh
+		sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./2-menu-scene-zelda.sh
+		sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.1-backup-restauracion.sh
+		sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2-retro-go-zelda.sh
+		sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.1-retro-go-4mb-zelda.sh
+		sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.2-cfw-retro-go-4mb-zelda.sh
+		sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.3-cfw-retro-go-16mb-zelda.sh
+		sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./scene/2.2.4-cfw-retro-go-64mb-zelda.sh
+		sed -i 's/^consola=.*$/'consola=\""zelda"\"'/g' ./menu.sh
+	./menu.sh
     clear;;
 esac
 clear
