d9ab22ba5170a127bf97be841ae777e1537d0595
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2021-12-08 18:52:46 +0100

    Update 2.2.1-retro-go-4mb-zelda.sh

--- scene/2.2.1-retro-go-4mb-zelda.sh
+++ scene/2.2.1-retro-go-4mb-zelda.sh
@@ -11,6 +11,11 @@ dialog --backtitle "G&W $consola - Utilidades de flasheo ------------------ INFO
 ans=$?
 if [ $ans -eq 0 ]; then
     clear
+	cd /home/$usuario/gameandwatch/game-and-watch-retro-go
+    make clean
+    make -j$proc COMPRESS=lzma GNW_TARGET=zelda
+    echo " "
+    echo " "
     echo " "
     echo -e "\e[1;34mSi ya has ejecutado esta opcion anteriormente y algo ha salido mal desmonta la consola y vuelve a ejecutar esta\e[0m"
     echo -e "\e[1;34mopcion y, al llegar a este punto, desconecta la bateria y vuelve a conectarla antes de realizar lo siguiente.\e[0m"
@@ -20,10 +25,12 @@ if [ $ans -eq 0 ]; then
     echo -e "\e[0;32mvamos a hacer un power cycle (quitar bateria, reconectar y encender) pulsaremos el boton de encendido y lo \e[0m"
     echo -e "\e[0;32mmantendremos pulsado unos segundos, le diremos que si con \"y\" (yes), entonces el proceso continuara.\e[0m"
     echo -e "\e[1;31mPulsa y manten pulsado el boton de encendido y justo despues pulsa cualquier tecla para continuar...\nATENCION: No sueltes el boton al menos hasta que empiece a borrar la memoria externa (cuando pone \"Erasing xxxx bytes...\" en la pantalla\e[0m"
-    read -n 1 -s -r -p ""
-    cd /home/$usuario/gameandwatch/game-and-watch-retro-go
-    make clean
-    make -j$proc COMPRESS=lzma GNW_TARGET=zelda flash
+    echo " "
+    read -n 1 -s -r -p "Pulsa cualquier tecla para continuar cuando lo tengas listo..."
+	make -j$proc COMPRESS=lzma GNW_TARGET=zelda flash
+    echo " "
+    echo " "
+    echo " "
     read -n 1 -s -r -p "Presiona cualquier tecla para continuar"
     dialog --backtitle "G&W - Utilidades de flasheo" \
     --title "Instalar solo Retro-Go" \
