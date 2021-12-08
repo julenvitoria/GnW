8e6b2e53ae494aa37e3421c7649d54e89d7217b8
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2021-12-08 02:48:31 +0100

    Update 2.2.6-actualizacion-parche.sh

--- scene/2.2.6-actualizacion-parche.sh
+++ scene/2.2.6-actualizacion-parche.sh
@@ -8,7 +8,7 @@ if [ -d /home/$usuario/gameandwatch/game-and-watch-patch ]; then
     cd /home/$usuario/gameandwatch/game-and-watch-patch
     git pull --recurse-submodules
     read -n 1 -s -r -p "Presiona cualquier tecla para continuar"
+    cd -
 else
     echo "El directorio del repo no se ha detectado, no se puede proseguir."
 fi
-cd -
