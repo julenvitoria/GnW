3a83457bb572e651529019113b470f642ed3049a
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2021-12-08 02:46:23 +0100

    Update 2.2.5-actualizacion-retro-go.sh

--- scene/2.2.5-actualizacion-retro-go.sh
+++ scene/2.2.5-actualizacion-retro-go.sh
@@ -7,8 +7,8 @@ clear
 if [ -d /home/$usuario/gameandwatch/game-and-watch-retro-go ]; then
     cd /home/$usuario/gameandwatch/game-and-watch-retro-go
     git pull --recurse-submodules
-        read -n 1 -s -r -p "Presiona cualquier tecla para continuar"
+    read -n 1 -s -r -p "Presiona cualquier tecla para continuar"
+    cd -
 else
     echo "El directorio del repo no se ha detectado, no se puede proseguir."
 fi
-cd -
