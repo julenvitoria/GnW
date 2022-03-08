a04c43d9144c19517dad298ff8b88cf8654b809b
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2022-03-08 03:08:20 +0100

    Update 1.4-openocd.sh

--- instalacion/1.4-openocd.sh
+++ instalacion/1.4-openocd.sh
@@ -11,12 +11,12 @@ sudo dpkg -i openocd-git_*_amd64.deb
 sudo apt-get -y -f install
 rm openocd*
 #echo export OPENOCD=/opt/openocd-git/bin/openocd >>~/.bashrc
-if grep -q "export OPENOCD=/opt/openocd-git/bin/openocd" /home/$usuario/.bashrc ; then
-        echo "export OPENOCD=/opt/openocd-git/bin/openocd esta ya en el bashrc"
+if grep -q "export OPENOCD=\"/opt/openocd-git/bin/openocd\"" /home/$usuario/.bashrc ; then
+        echo "export OPENOCD=\"/opt/openocd-git/bin/openocd\" esta ya en el bashrc"
 else
-        echo "export OPENOCD=/opt/openocd-git/bin/openocd no esta aun en el bashrc, añadiendo..."
+        echo "export OPENOCD=\"/opt/openocd-git/bin/openocd\" no esta aun en el bashrc, añadiendo..."
         sleep 2
-        echo export OPENOCD=/opt/openocd-git/bin/openocd >>/home/$usuario/.bashrc
+        echo export OPENOCD=\"/opt/openocd-git/bin/openocd\" >>/home/$usuario/.bashrc
 fi
 
 export OPENOCD="/opt/openocd-git/bin/openocd"
