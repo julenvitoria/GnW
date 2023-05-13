edc080f690b46e95ac56c182dcaa919fd8891825
julenvitoria <julenvitoria@hotmail.es>
2023-05-13 12:44:28 +0200

    se repara menu scene zelda

--- scene/2.2-retro-go-zelda.sh
+++ scene/2.2-retro-go-zelda.sh
@@ -4,7 +4,7 @@
 INPUT=/tmp/$MENU.sh.$$
 usuario="kde"
 consola="zelda"
-proc="4"
+proc="6"
 caratula="0"
 
 dialog --backtitle "G&W $consola - Utilidades de flasheo ------------------ INFO: 2.2-retro-go-zelda.sh Usuario = $usuario   ////   Consola seleccionada = $consola ------------------" \
